03dd7540886b3c46d01f31eb222cb2c67b263283
Martin Björkström <martin.bjorkstrom@gmail.com>
2022-10-14 15:58:31 +0300

    prt 2

--- day08.fsx
+++ day08.fsx
@@ -1,3 +1,4 @@
+open System
 open System.IO
 
 type Line = {
@@ -33,6 +34,8 @@ let parseFile file =
     |> File.ReadAllLines
     |> Seq.map parseLine
 
+// Part 1
+
 let part1 file =
     file
     |> parseFile
@@ -40,3 +43,43 @@ let part1 file =
     |> Seq.sum
 
 part1 "day08.txt"
+
+// Part 2
+
+type LineExt = {
+    Original : Line
+    Encoded : Line
+}
+
+let encode (str : string) =
+    let rec loop (acc : string) (str : char list) =
+        match str with
+        | [] -> acc
+        | char::tail ->
+            let (add, skip) =
+                match char with
+                | '\\' ->   if tail.[0] = 'x' then
+                                (@"\\" + String(tail.[0..2] |> List.toArray), 3)
+                            else
+                                (@"\\", 0)
+                | '"' -> (@"\""", 0)
+                | _ -> (char.ToString(), 0)
+            let acc = acc + add
+            loop acc (tail |> List.skip skip)
+    str.ToCharArray()
+    |> List.ofArray
+    |> loop ""
+    |> sprintf "\"%s\"" // Add the quotes
+
+let encodeLine line =
+    let encoded = line.Text |> encode
+    let size = encoded |> calcSize
+    { Original = line; Encoded = { Text = encoded; Length = encoded.Length; Size = size } }
+
+let part2 file = 
+    file
+    |> parseFile
+    |> Seq.map encodeLine
+    |> Seq.sumBy (fun l -> l.Encoded.Length - l.Original.Length)
+
+part2 "day08.txt"
