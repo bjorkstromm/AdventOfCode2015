36813d55553735480ad762bd4595baf758b3a763
Martin Björkström <martin@bjorkstrom.dev>
2022-11-08 21:34:26 +0200

    Day 13 - Part 2

--- day13.fsx
+++ day13.fsx
@@ -42,13 +42,18 @@ let rec permute lst =
 
 let calcHappiness (rules : Map<string, Map<string, int>>) seating =
     let happiness i =
+        let happ r x =
+            match r |> Map.tryFind x with
+            | Some i -> i
+            | _ -> 0
+
         let li = (if i = 0 then List.length seating else i) - 1
         let ri = if i + 1 = List.length seating then 0 else i + 1
         let l = seating.[li]
         let r = seating.[ri]
 
         let rule = rules.[seating.[i]]
-        rule.[l] + rule.[r]
+        happ rule l + happ rule r
 
     seating
     |> List.mapi (fun i _ -> happiness i)
@@ -69,5 +74,22 @@ let part1 filename =
 
     optimal
 
-"test.txt" |> part1
-"day13.txt" |> part1
+let part2 filename = 
+    let rules =
+        filename
+        |> getRules
+        |> Map.add "Me" Map.empty
+    let first = rules.Keys |> Seq.head
+    let optimal =
+        rules.Keys
+        |> List.ofSeq
+        |> permute
+        |> Seq.filter (fun xs -> xs |> List.head = first)
+        |> Seq.map (calcHappiness rules)
+        |> Seq.sortDescending
+        |> Seq.head
+
+    optimal
+
+"test.txt" |> part2
+"day13.txt" |> part2
